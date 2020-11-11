03cc685ecdba5e98d459f21c6c0fceab06c16aa8
javiertxu18 <javierherreroprietopc@hotmail.com>
2020-11-11 13:44:14 +0100

    Conn SQL Server

--- app/src/main/java/com/example/reto1android/MainActivity.java
+++ app/src/main/java/com/example/reto1android/MainActivity.java
@@ -3,21 +3,36 @@ package com.example.reto1android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import java.sql.Connection;
 
 import connSqlServer.ConnSqlServer;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected Connection conn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //AsyncTask de
+        ConnSqlServer admin = new ConnSqlServer("10.0.2.2" , "1433" , "PruebaAndroid" , "JHERRERO-P\\JAVISQL" ,  "admin" , "Admin1234");
+        admin.execute();
+    }
+
+    public void sysoConn(View view){
+        System.out.println("Hola2: " + conn);
     }
 
     public void connDb(View view){
-        ConnSqlServer conn = new ConnSqlServer();
-        conn.connDb();
+        try {
+            conn.close();
+        }catch (Exception e){
+            Log.e("Error en cierre de connexión a BD: " , ""+e);
+        }
     }
 }
--- app/src/main/java/connSqlServer/ConnSqlServer.java
+++ app/src/main/java/connSqlServer/ConnSqlServer.java
@@ -1,13 +1,44 @@
 package connSqlServer;
 
+import android.os.AsyncTask;
 import android.os.StrictMode;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.example.reto1android.MainActivity;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class ConnSqlServer {
-    public void connDb(){
+public class ConnSqlServer extends AsyncTask<Integer , Integer , Connection> {
+    //Attribs
+    protected  String server;
+    protected  String port;
+    protected  String db;
+    protected  String instance;
+    protected  String user;
+    protected  String passwd;
+    protected  String connUrl;
+    protected  Connection conn;
+
+    //Getters & Setters
+    public Connection getConn(){
+        return this.conn;
+    }
+
+    //Methods
+    @Override
+    protected Connection doInBackground(Integer... integers) {
+        this.conn = this.connDb();
+        return this.conn;
+    }
+
+    @Override
+    protected void onPostExecute(Connection s) {
+        super.onPostExecute(s);
+    }
+
+    public Connection connDb(){
         System.out.println("Conectando a la BD ....");
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -15,13 +46,28 @@ public class ConnSqlServer {
 
         try {
             Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
-            //old: "jdbc:jtds:sqlserver://127.0.0.1:1433/PruebaAndroid;instance=JAVISQL;user=admin;password=Admin1234"
-            String url  = "jdbc:jtds:sqlserver://10.0.2.4:1433;databaseName=PruebaAndroid;instance=JHERRERO-P\\JAVISQL;user=admin;password=Admin1234";
-            Connection conn = DriverManager.getConnection(url);
+            //old: "jdbc:jtds:sqlserver://127.0.0.1:1433/PruebaAndroid;instance=JAVISQL;user=admin;password=Admin1234" //10.0.2.2
+            //String url  = "jdbc:jtds:sqlserver://"+_server+":"+_port+";databaseName="+_db+";instance="+_instance+";user="+_user+";password="+_passwd+";";
+            System.out.println(this.connUrl);
+            Connection conn = DriverManager.getConnection(this.connUrl);
             System.out.println("Conexión establecida.");
+            return conn;
         }catch (Exception e){
             Log.e("Error al intentar conectar con SQL Server -> ", ""+e);
             System.out.println("No se ha establecido conexión con la BD.");
         }
+        return null;
+    }
+
+    //Constructores
+    public ConnSqlServer(String _server , String _port , String _db , String _instance , String _user , String _passwd){
+        this.server = _server;
+        this.port = _port;
+        this.db = _db;
+        this.instance = _instance;
+        this.user = _user;
+        this.passwd = _passwd;
+        this.connUrl = "jdbc:jtds:sqlserver://"+_server+":"+_port+";databaseName="+_db+";instance="+_instance+";user="+_user+";password="+_passwd+";";
+        this.conn = null;
     }
 }
